244fb597c6056c678f2735f28c33fc400cad1f19
Makhmutov Maxim <makhmutov@gmail.com>
2025-01-05 13:01:57 +0700

    добовления кнопки изменения todo в задание 2(json-server)

--- src/App.js
+++ src/App.js
@@ -1,145 +1,124 @@
 import React, { useState, useEffect } from 'react';
-import {
-	BrowserRouter as Router,
-	Route,
-	Routes,
-	Link,
-	useNavigate,
-	useParams,
-} from 'react-router-dom';
 import axios from 'axios';
 import s from './style.module.css';
-
-const Home = () => {
+export const App = () => {
 	const [todos, setTodos] = useState([]);
 	const [newTodo, setNewTodo] = useState('');
 	const [search, setSearch] = useState('');
 	const [isSort, setIsSort] = useState(false);
-
+	const [isLoading, setIsLoading] = useState(true);
+	const [editingTodo, setEditingTodo] = useState(null);
+	// Загрузка данных из JSON Server
 	useEffect(() => {
-		axios.get('http://localhost:3005/todos').then((res) => setTodos(res.data));
+		const fetchTodos = async () => {
+			try {
+				const response = await axios.get('http://localhost:3005/todos');
+				setTodos(response.data);
+			} catch (error) {
+				console.error('Ошибка при загрузке данных:', error);
+			} finally {
+				setIsLoading(false); // Устанавливаем isLoading в false после загрузки
+			}
+		};
+		fetchTodos();
 	}, []);
 
-	const addTodo = () => {
-		axios.post('http://localhost:3005/todos', { title: newTodo }).then((res) => {
-			setTodos([...todos, res.data]);
-			setNewTodo('');
-		});
+	const startEditing = (todo) => {
+		setEditingTodo({ ...todo }); // Устанавливаем редактируемый todo
 	};
 
+	const saveTodo = async () => {
+		await axios.put(`http://localhost:3005/todos/${editingTodo.id}`, editingTodo);
+		setTodos(todos.map((todo) => (todo.id === editingTodo.id ? editingTodo : todo)));
+		setEditingTodo(null); // Сбрасываем редактирование
+	};
+
+	// Добавление нового дела
+	const addTodo = async () => {
+		const response = await axios.post('http://localhost:3005/todos', {
+			title: newTodo,
+		});
+		setTodos([...todos, response.data]);
+		setNewTodo('');
+	};
+	// Удаление дела
+	const deleteTodo = async (id) => {
+		await axios.delete(`http://localhost:3005/todos/${id}`);
+		setTodos(todos.filter((todo) => todo.id !== id));
+		console.log(id);
+	};
 	const filteredTodos = todos.filter((todo) => todo.title.includes(search));
 	const sortedTodos = isSort
 		? [...filteredTodos].sort((a, b) => a.title.localeCompare(b.title))
 		: filteredTodos;
-
 	return (
 		<div className={s.container}>
 			<div className={s.header}>
 				<h1>Список дел</h1>
 			</div>
-			<input
-				placeholder="Напишите дело..."
-				className={s.inp}
-				value={newTodo}
-				onChange={(e) => setNewTodo(e.target.value)}
-			/>
-			<button className={s.btn} onClick={addTodo}>
-				Добавить
-			</button>
-			<input
-				className={s.inp}
-				value={search}
-				onChange={(e) => setSearch(e.target.value)}
-				placeholder="Поиск..."
-			/>
-			<button className={s.btn} onClick={() => setIsSort(!isSort)}>
-				Сортировать
-			</button>
-			<ul>
-				{sortedTodos.map((todo) => (
-					<li key={todo.id}>
-						<Link to={`/task/${todo.id}`}>
-							{todo.title.length > 20
-								? `${todo.title.slice(0, 20)}...`
-								: todo.title}
-						</Link>
-					</li>
-				))}
-			</ul>
+			{isLoading ? (
+				<div className={s.loader}></div>
+			) : (
+				<div>
+					<input
+						value={newTodo}
+						className={s.inp}
+						placeholder="Напишите дело"
+						onChange={(e) => setNewTodo(e.target.value)}
+					/>
+					<button className={s.btn} onClick={addTodo}>
+						Добавить
+					</button>
+					<input
+						className={s.inp}
+						value={search}
+						onChange={(e) => setSearch(e.target.value)}
+						placeholder="Поиск..."
+					/>
+					<button className={s.btn} onClick={() => setIsSort(!isSort)}>
+						Сортировать
+					</button>
+					<ul className={s.container}>
+						{sortedTodos.map((todo) => (
+							<li key={todo.id}>
+								{editingTodo?.id === todo.id ? (
+									<input
+										value={editingTodo.title}
+										onChange={(e) =>
+											setEditingTodo({
+												...editingTodo,
+												title: e.target.value,
+											})
+										}
+									/>
+								) : (
+									todo.title
+								)}
+								<div>
+									<button
+										className={s.btn}
+										onClick={() => deleteTodo(todo.id)}
+									>
+										Удалить
+									</button>
+									{editingTodo?.id === todo.id ? (
+										<button className={s.btn} onClick={saveTodo}>
+											Сохранить
+										</button>
+									) : (
+										<button
+											className={s.btn}
+											onClick={() => startEditing(todo)}
+										>
+											Изменить
+										</button>
+									)}
+								</div>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 };
-
-const Task = () => {
-	const { id } = useParams();
-	const [todo, setTodo] = useState(null);
-	const [editMode, setEditMode] = useState(false);
-	const [editedTitle, setEditedTitle] = useState('');
-	const navigate = useNavigate();
-
-	useEffect(() => {
-		axios.get(`http://localhost:3005/todos/${id}`).then((res) => setTodo(res.data));
-	}, [id]);
-
-	const deleteTodo = () => {
-		axios.delete(`http://localhost:3005/todos/${id}`).then(() => navigate('/'));
-	};
-
-	useEffect(() => {
-		axios.get(`http://localhost:3005/todos/${id}`).then((res) => {
-			setTodo(res.data);
-			setEditedTitle(res.data.title);
-		});
-	}, [id]);
-
-	const updateTodo = () => {
-		axios
-			.put(`http://localhost:3005/todos/${id}`, { title: editedTitle })
-			.then(() => {
-				setTodo({ ...todo, title: editedTitle });
-				setEditMode(false);
-			});
-	};
-
-	return todo ? (
-		<div className={s.todo_container}>
-			<button className={s.btn} onClick={() => navigate(-1)}>
-				Назад
-			</button>
-			{editMode ? (
-				<>
-					<input
-						className={s.inp}
-						value={editedTitle}
-						onChange={(e) => setEditedTitle(e.target.value)}
-					/>
-					<button className={s.btn} onClick={updateTodo}>
-						Сохранить
-					</button>
-				</>
-			) : (
-				<>
-					<h4>{todo.title}</h4>
-					<button className={s.btn} onClick={() => setEditMode(true)}>
-						Редактировать
-					</button>
-				</>
-			)}
-			<button className={s.btn} onClick={deleteTodo}>
-				Удалить
-			</button>
-		</div>
-	) : null;
-};
-
-export const App = () => (
-	<Router>
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/task/:id" element={<Task />} />
-			<Route path="*" element={<div>404 Not Found</div>} />
-		</Routes>
-	</Router>
-);
-
-export default App;
